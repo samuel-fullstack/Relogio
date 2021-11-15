568c45491aecfa6b52c172e65d8a37d320c9a282
samuel santos <samueldasilva2209@gmail.com>
2021-11-15 12:00:05 -0300

    adiciona zero quando minutos segundos e horas forem menor que 10

--- hora.js
+++ hora.js
@@ -6,6 +6,22 @@ function puxaHora() {
     let minutos = currentDay.getMinutes();
     let segundos = currentDay.getSeconds();
 
+
+    if(segundos < 10){
+
+        segundos=`0${segundos}`
+
+    }
+    if(minutos < 10){
+
+        minutos=`0${minutos}`
+
+    }
+    if(horas < 10){
+
+        horas =`0${horas}`
+
+    }
    
     document.getElementById('hora').innerHTML =`${horas} : ${minutos} : ${segundos}` 
     
